4404aa7abf909f3262b6ba28e088106c2729e309
Thomi55 <thomyeze57@gmail.com>
2022-02-21 14:35:33 -0300

    nuevo js

--- codigo.js
+++
@@ -1,3 +0,0 @@
-let numero = 23;
-
-alert(numero)
--- js/main.js
+++ js/main.js
@@ -1,59 +1,43 @@
-/*
-let bebida
-let descuento = prompt('Si contestas bien a este calculo, te ganas un descuento del 25% \nescribe Ok para avanzar. ')
-const acertijo = () => {
-let cuenta = prompt("5+5?");
-if (cuenta === '10') {
-    bebida = prompt('Felicidades, contestaste correctamente \nElija que quiere beber entre agua o coca')
-    return bebida
-}
-else alert("Te equivocaste, perdiste el descuento. ")
-}
-
-acertijo()
-
-const viajePrecio = prompt("Cada bebida vale 300 \nEscriba el precio que mencionamos arriba para finalizar su compra")
-
-const resta = (precio,descuento) =>   `${bebida} ${precio - descuento}`
-
-alert(resta(300,75));
-}
-*/
-class Bebida { 
-    constructor(id,nombre,precio) {
-        this.id= parseInt(id);
-        this.nombre= nombre;
-        this.precio= parseFloat(precio);
+class Contacto {
+    constructor(nombre,numero,direccion){
+        this.nombre=nombre;
+        this.numero=numero;
+        this.direccion=direccion;
     }
 }
-const bebidas =[];
-bebidas.push(new Bebida(1,'Licuado de Banana',250));
-bebidas.push(new Bebida(2,'Licuado de Frutilla',250));
-bebidas.push(new Bebida(2,'Daiquiri',300));
-bebidas.push(new Bebida(3,'Fernet',300));
-bebidas.push(new Bebida(3,'Jagermeister',400));
-console.log(bebidas);
 
-let divBeb = document.getElementById("proximo");
+const contactos = [];
 
-for (const bebida of bebidas) {
-    let divBebida = document.createElement("div")
-    divBebida.innerHTML=`<h2>Bebida: ${bebida.nombre}</h2>
-                        <h2>Precio: $ ${bebida.precio} </h2>
-                        <button id='${bebida.id}' class='botonComprar'>Precomprar</button>
-                        <hr> `;
-    divBeb.append(divBebida);
+let guardarContacto = document.getElementById("guardarContacto");
+
+let divSolicitud = document.getElementById("solicitud");
+
+if ("Contactos" in localStorage) {
+    const contactoGuardado = JSON.parse(localStorage.getItem("Contactos"));
+    console.log(contactoGuardado);
+    for (const generico of contactoGuardado) {
+        contactos.push(new Contacto(generico.nombre,generico.numero,generico.direccion))
+    }
+    console.log(contactos);
+    interfaz(contactos);
 }
 
-let button = document.getElementsByClassName('botonComprar')
-console.log(button);
-
-for (const boton of button) {
-    boton.addEventListener("click", function () {
-        console.log(this.id);
-        let encontrar = bebidas.find(bebida => bebida.id==this.id);
-        console.log(encontrar);
-    })
-    
+guardarContacto.onsubmit = (e) =>{
+    e.preventDefault();
+    let hijos = guardarContacto.children
+    console.log(hijos);
+    contactos.push(new Contacto(hijos[0].value, hijos[1].value, hijos[2].value));
+    console.log(contactos);
+    interfaz(contactos)
+    e.target.reset();
+    localStorage.setItem("Contactos",JSON.stringify(contactos));    
 }
 
+function interfaz (lista){
+    divSolicitud.innerHTML="";
+    for (const contacto of lista) {
+    let divContacto= document.createElement("div")
+    divContacto.innerHTML= `Nombre: ${contacto.nombre} Numero de celular: ${contacto.numero} Direccion: ${contacto.direccion}`
+    divSolicitud.append(divContacto)
+    }
+}   
